e41e69659c238510d3196087f7ea5c9718ed30e8
abdelaziz-elshrkawy <abdelaziz_elshrkawy53@yahoo.com>
2024-08-26 10:04:42 +0300

    updating file name in command.js file

--- command.js
+++ command.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-const {Command} = require('commander');
-const {existsSync} = require('fs');
+const { Command } = require('commander');
+const { existsSync } = require('fs');
 const path = require('path');
-const {generateLottieJSON} = require('./animation');
+const { generateLottieJSON } = require('./lottieConstructor');
 
 
 
@@ -10,20 +10,20 @@ const program = new Command();
 
 
 program
-.version('1.0.0')
-.description('Convert PNG frames to Lottie JSON format')
-.option('-i, --input <dir>', 'Input directory containing PNG frames', './')
-.option('-o, --output <file>', 'Output Lottie JSON file', 'output.json')
-.option('-f, --framerate <rate>', 'Frame rate of the animation', '24');
+    .version('1.0.0')
+    .description('Convert PNG frames to Lottie JSON format')
+    .option('-i, --input <dir>', 'Input directory containing PNG frames', './')
+    .option('-o, --output <file>', 'Output Lottie JSON file', 'output.json')
+    .option('-f, --framerate <rate>', 'Frame rate of the animation', '24');
 
 program.parse(process.argv);
 
-const {input, output, framerate} = program.opts();
+const { input, output, framerate } = program.opts();
 
-if(!existsSync(path.join(__dirname, input))){
+if (!existsSync(path.join(__dirname, input))) {
     console.error('Error: path is not valid or directory doesn\'t exists')
-}else{
-    generateLottieJSON(input, output, framerate).catch(e=>console.error(e));
+} else {
+    generateLottieJSON(input, output, framerate).catch(e => console.error(e));
 }
-    
+
 
